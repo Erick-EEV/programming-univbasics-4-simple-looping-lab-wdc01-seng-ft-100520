384db44c4cdcd118914bdd0c76c52c3212e394fd
Erick Villeda <eeverickeev@gmail.com>
2020-09-02 13:19:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,13 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  5.times {puts message}
+end
+
+def loop_message_n_times(message, integer)
+  integer.times {puts message}
+end
+
+def output_array(array)
+  
+end
